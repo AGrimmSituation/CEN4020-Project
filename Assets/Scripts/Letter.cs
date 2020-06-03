4ee1691c3eb2312bec1df10582538db877da53c6
AGrimmSituation <46505357+AGrimmSituation@users.noreply.github.com>
2020-06-02 21:13:08 -0400

    Added script for mail

---
+++ Assets/Scripts/Letter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Letter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("test");
+    }
+}
