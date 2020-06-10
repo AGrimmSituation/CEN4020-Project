9be78c399a01b12f8d5f1d8715daf56b87cface6
freyt <jcsardap1998@gmail.com>
2020-06-10 06:14:08 -0400

    Clean up Clock logic and code

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    [SerializeField]GameObject UItoShow = null;
+    [SerializeField] GameObject UItoShow = null;
+
     void OnMouseDown()
     {
         if (UItoShow)
