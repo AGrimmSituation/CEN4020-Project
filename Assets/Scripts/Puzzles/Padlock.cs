b86afa7919c55d8421affd24b93fbd2fd72408a0
freyt <jcsardap1998@gmail.com>
2020-06-18 09:45:44 -0400

    Implement padlock text, rename scene

--- Assets/Scripts/Puzzles/Padlock.cs
+++ Assets/Scripts/Puzzles/Padlock.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Padlock : MonoBehaviour
 {
     [Header("Scene References")]
     [SerializeField] GameObject door = null;
     [SerializeField] GameObject padlock = null;
+    [SerializeField] Text text = null;
 
     [Header("Attributes")]
     [SerializeField] string answer = "2973";
@@ -12,6 +14,11 @@ public class Padlock : MonoBehaviour
 
     bool isUnlocked = false;
     public bool IsUnlocked { get => isUnlocked; }
+
+    void Start()
+    {
+        text.text = "";
+    }
 
     public void AddDigit(string s)
     {
@@ -29,6 +36,11 @@ public class Padlock : MonoBehaviour
             {
                 input = "";
             }
+        }
+
+        if (text)
+        {
+            text.text = input;
         }
     }
 }
