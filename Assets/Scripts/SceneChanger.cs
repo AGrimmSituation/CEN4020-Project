5f5da17be8d8b60c74d28aae84e99d1f2f9a1028
AGrimmSituation <46505357+AGrimmSituation@users.noreply.github.com>
2020-07-03 23:43:44 -0400

    Editing kitchen door
    Changed position of door in kitchen scene, added script to trigger scene change.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneChanger : MonoBehaviour
+{
+    //Apply to a door object
+    //When a door is clicked on, this script will load the 
+    //level attached to the door.
+
+    [SerializeField] string sceneName = null;
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
