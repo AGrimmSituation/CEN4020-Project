d0243ddffe3d4627c1cfe1b10d6c8107a14b6c8c
freyt <jcsardap1998@gmail.com>
2020-07-06 17:17:39 -0400

    Implement basic TV Puzzle

---
+++ Assets/Scripts/Puzzles/TV/TvKnob.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class TvKnob : MonoBehaviour
+{
+    bool operatable = true;
+    int curState = 0;
+    int maxState = 10;
+
+    public int CurState { get => curState; }
+
+    void OnMouseDown()
+    {
+        if (operatable)
+        {
+            curState = (curState + 1) % maxState;
+            transform.eulerAngles = new Vector3(0, 0, 360 * ((float)curState / (float)maxState) );
+        }
+    }
+
+    public void Deactivate()
+    {
+        operatable = false;
+    }
+}
---
+++ Assets/Scripts/Puzzles/TV/TvPuzzle.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class TvPuzzle : MonoBehaviour
+{
+    [Header("Scene References")]
+    [SerializeField] TvKnob channelKnob = null;
+    [SerializeField] TvKnob volumeKnob = null;
+    [SerializeField] SpriteRenderer solvedChannel = null;
+
+    bool isSolved = false;
+
+    [Header("Attributes")]
+    [SerializeField] [Range(0, 10)] int correctVolume = 0;
+    [SerializeField] [Range(0, 10)] int correctChannel = 0;
+
+    void Update()
+    {
+        if (!isSolved && 
+            channelKnob && channelKnob.CurState == correctChannel && 
+            volumeKnob && volumeKnob.CurState == correctVolume)
+        {
+            isSolved = true;
+            channelKnob.Deactivate();
+            volumeKnob.Deactivate();
+            solvedChannel.gameObject.SetActive(true);
+        }
+    }
+}
