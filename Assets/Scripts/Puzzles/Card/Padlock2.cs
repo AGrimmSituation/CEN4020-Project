4aa4160f5b88101f39e59e55b6035e7fe54002d0
aloraclark <65735963+aloraclark@users.noreply.github.com>
2020-07-08 23:27:47 -0400

    Scripts for Bedroom Padlock
    Coded the scripts for the bedroom padlock based on previous code written for the living room padlock. Should work exactly the same. I also created a folder for these scripts under puzzles, inside "Card".

---
+++ Assets/Scripts/Puzzles/Card/Padlock2.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Padlock2 : MonoBehaviour
+{
+    //Code from previous padlock implementation
+    //edited to work with padlock for this scene
+
+    [Header("Scene References")]
+    [SerializeField] GameObject door = null;
+    [SerializeField] GameObject padlock = null;
+    [SerializeField] Text text = null;
+
+
+    [Header("Attributes")]
+    [SerializeField] string answer = "understand";
+    string input = "";
+
+    bool isUnlocked = false;
+    public bool IsUnlocked { get => isUnlocked; }
+
+    void Start()
+    {
+        text.text = "";
+    }
+
+    
+    public void AddLetter(string s)
+    {
+        //while the padlock is locked, take in the answer combination
+        //the user supplies and check against the correct answer.
+        //if the answer is correct, unlock the padlock and hide it 
+        //from view. this allows the user to go through the door
+        //with the padlock now open
+
+        if(isUnlocked==false)
+        {
+            input += s;
+            if(input==answer)
+            {
+                isUnlocked = true;
+                door.GetComponent<BoxCollider2D>().enabled = true;
+                padlock.SetActive(false);
+                gameObject.SetActive(false);
+            }
+            else if (input.Length >= 10)
+            {
+                input = "";
+            }
+        }
+
+        if(text)
+        {
+            text.text = input;
+        }
+    }
+}
---
+++ Assets/Scripts/Puzzles/Card/PadlockButton2.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PadlockButton2 : MonoBehaviour
+{
+    //Code used from previous padlock button implementation
+
+    [SerializeField] Padlock2 padlock = null;
+    [SerializeField] string letter = "a";
+
+    //each time the user clicks on a letter,
+    //the letter will be sent to the "Padlock2"
+    //script to compare to the correct answer
+
+    void OnMouseDown()
+    {
+        if(padlock)
+        {
+            padlock.AddLetter(letter);
+        }
+    }
+}
