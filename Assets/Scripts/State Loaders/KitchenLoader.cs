d959e427fe776e3a5274033f8747ce77cea11c62
freyt <jcsardap1998@gmail.com>
2020-07-22 11:48:32 -0400

    Revert "Remove old savestate system"
    This reverts commit f71c7804f97cc6a8e1285e9aab1e3dfa8ca6b67f.

---
+++ Assets/Scripts/State Loaders/KitchenLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenLoader : MonoBehaviour
+{
+    [SerializeField] FridgePuzzle fridgePuzzle = null;
+
+    void Start()
+    {
+        if (SavedState.fridgeSolved)
+        {
+            fridgePuzzle.SolveAndLock();
+        }
+    }
+}
