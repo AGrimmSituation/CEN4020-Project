b6c02b8386dd48d2f73d2eda9e249a48c04f11db
AGrimmSituation <46505357+AGrimmSituation@users.noreply.github.com>
2020-07-27 15:26:43 -0400

    Title Screen
    Added a title screen scene. This will allow the player to start a new game with no save data or continue from their most recent save. They can also close the program from here. Currently has support for loading the living room fresh and should be able to close. Need to implement a method for reaching this screen in-game.

---
+++ Assets/Scripts/CloseGame.cs
@@ -0,0 +1,15 @@
+﻿//When clicking "Quit" option in menu, this script will close the program.
+
+using System.Collections;
+using UnityEngine;
+
+public class CloseGame : MonoBehaviour
+{
+    [SerializeField] GameObject MenuItem = null;
+    //Click "Quit" to trigger Quit function.
+    void OnMouseDown()
+    {
+        if (MenuItem)
+            Application.Quit();
+    }
+}
---
+++ Assets/Scripts/OpenGameMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OpenGameMenu : MonoBehaviour
+{
+    //Attaches a UI to this function call.
+    [SerializeField] GameObject UItoShow = null;
+    // Update is called once per frame. It will check to see if the menu button is called. We use escape as it is the most common menu button in similar applications
+    void Update()
+    {
+        if(Input.GetKey("escape"))  //If the player presses escape key, attempt to open menu.
+        {
+            if (UItoShow)           //If there is no menu to open, nothing will happen.
+                UItoShow.SetActive(true);
+        }
+    }
+}
