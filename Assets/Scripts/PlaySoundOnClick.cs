0d08a3614d8e54c47621fb6587f1d91147cd0547
JacobPetrillo <jmp16@my.fsu.edu>
2020-07-27 17:31:01 -0400

    Create PlaySoundOnClick.cs

---
+++ Assets/Scripts/PlaySoundOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlaySoundOnClick : MonoBehaviour
+{
+    [SerializeField]AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    void OnMouseDown()
+    {
+        sound.PlayOneShot(sound.clip);
+    }
+}
