e6cd1f239636cb98187ce95e8ddebd9b1f8382e4
JacobPetrillo <jmp16@my.fsu.edu>
2020-07-28 21:58:49 -0400

    Create NewGame.cs

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+	void OnMouseDown()
+	{
+	SaveSystem.NewGame();
+	SceneManager.LoadScene(SavedState.lastScene);
+	}
+}
