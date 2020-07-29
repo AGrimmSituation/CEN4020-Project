2f1d560503837aa4ddd7b66535cbba9c03ae9e89
aloraclark <65735963+aloraclark@users.noreply.github.com>
2020-07-29 18:38:28 -0400

    Fixed TV audio

--- Assets/Scripts/Puzzles/TV/StaticHandler.cs
+++ Assets/Scripts/Puzzles/TV/StaticHandler.cs
@@ -1,23 +1,34 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class StaticHandler : MonoBehaviour
 {
     [SerializeField]AudioSource sound = null;
     [SerializeField]SpriteRenderer solvedChannel = null;
+    public AudioClip noise1;
+    public AudioClip noise2;
+
     // Start is called before the first frame update
     void Start()
     {
         sound = GetComponent<AudioSource>();
+	sound.clip = noise1;
         sound.playOnAwake = true;
+	sound.clip = noise2;
+    	sound.loop = true;
+    	sound.Play();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+
         if(solvedChannel.gameObject.activeSelf == true)
         {
+	    sound.loop = false;
             sound.Stop();
         }
     }
