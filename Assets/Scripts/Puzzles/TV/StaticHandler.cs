b5f914ab5decd8df65dfe673858575be15176881
AGrimmSituation <46505357+AGrimmSituation@users.noreply.github.com>
2020-07-29 12:45:21 -0400

    Attempting to fix static tv glitch

---
+++ Assets/Scripts/Puzzles/TV/StaticHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticHandler : MonoBehaviour
+{
+    [SerializeField]AudioSource sound = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+        sound.playOnAwake = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SavedState.tvSolved == true)
+        {
+            sound.playOnAwake = false;
+            sound.loop = false;
+            sound.Stop();
+        }
+    }
+}
