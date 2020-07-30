1ba68903d61e73aac2b67e41220fa43b0f612086
MelanieBynum <65685320+MelanieBynum@users.noreply.github.com>
2020-07-29 21:03:37 -0400

    started bikelock puzzle
    began the bikelock puzzle, added comments to allow other members to continue it and give suggestions.

---
+++ Assets/Scripts/Puzzles/Bathroom/BikeLock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BikeLock : MonoBehaviour
+{
+    [Header("Scene References")]
+    [SerializeField] GameObject cabinet = null;
+    [SerializeField] GameObject bikelock = null;
+    [SerializeField] Text text = null;
+
+    [Header ("Attributes")]
+    [SerializeField] string answer = "531";
+    string input = "";
+
+    void Start()
+    {
+        //text will show the different numbers
+        //on the bikelock
+        text.text = "";
+    }
+
+    public void AddDigit(string str)
+    {
+        //while unsolved, compile answers
+        //and check if they're correct
+
+        //if the input size > 3, reset it
+        //if you want to change the length, feel free
+    }
+
+    public void SolveAndOpen()
+    {
+        //once solved, unlock the bikelock
+        //and allow the cabinet to be opened
+    }
+}
