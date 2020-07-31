e93c4431083ab5706ddb72f80b639f19edb2517d
freyt <jcsardap1998@gmail.com>
2020-07-30 22:01:31 -0400

    Add code to unlock loft

--- Assets/Scripts/State Loaders/LivingRoomLoader.cs
+++ Assets/Scripts/State Loaders/LivingRoomLoader.cs
@@ -6,17 +6,32 @@ public class LivingRoomLoader : MonoBehaviour
 {
     [SerializeField] Clock clockPuzzle = null;
     [SerializeField] Padlock padlockPuzzle = null;
+    [SerializeField] BoxCollider2D loftDoor = null;
 
     void Start()
     {
         if (SavedState.clockSolved)
         {
-            clockPuzzle.SolveAndLock();
+            if (clockPuzzle)
+            {
+                clockPuzzle.SolveAndLock();
+            }
         }
 
         if (SavedState.livingRoomPadlockSolved)
         {
-            padlockPuzzle.SolveAndLock();
+            if (padlockPuzzle)
+            {
+                padlockPuzzle.SolveAndLock();
+            }
+        }
+
+        if (SavedState.tvSolved)
+        {
+            if (loftDoor)
+            {
+                loftDoor.GetComponent<BoxCollider2D>().enabled = true;
+            }
         }
     }
 }
